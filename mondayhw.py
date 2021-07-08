d3e5d303449fc1b90815d98873ffffb94e791f42
krstfr <86634782+krstfr@users.noreply.github.com>
2021-07-07 21:27:51 -0700

    Add files via upload

---
+++ mondayhw.py
@@ -0,0 +1,41 @@
+###Exercise 1: %Cube Number Test... Print out all cubed numbers up to the 
+total value 1000, so if the cubed number is over 1000 break the loop.
+
+My Answer: 
+for i in range (10):
+    if i == 1000:
+        print("i equals 1000")
+        break
+
+
+
+##Exercise 2: Get first prime numbers up to 100
+
+My Answer: 
+numbers = range (99)
+range (0,99)
+for number in numbers:
+    if number % 2 !=0
+    print( number )
+
+
+###Exercise 3: Take in a users input for their age, if they are younger than 
+18 print kids, if they're 18 to 65 print adults, else print seniors####
+
+My Answer: 
+age = input("What is your age?")
+print(type(age))
+num10 = 18
+num11 = 65
+
+if int(age)< num10:
+    print ("kids")
+    
+else:
+    print ("adults")
+
+if int(age)>= num11:
+    print ("seniors")
+
+else:
+    print ("adults")
